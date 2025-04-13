ed7a8f107f92d2e2be4c501e7df2bfe87d448dab
Owin <owin@owinrojas.com>
2025-04-13 02:43:44 -0700

    testing websockets implementation

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+import cv2
+import numpy as np
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 # Camera Test Page
 @app.route('/')
 def index():
     return render_template('CameraTest.html')
+
+@socketio.on('frame')
+def handle_frame(data):
+    nparr = np.frombuffer(data, np.uint8)
+    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
+    if frame is not None:
+        cv2.imshow('WebSocket Frame', frame)
+        if cv2.waitKey(1) == 27:  # ESC
+            exit(0)
 
 # Run
 if __name__ == '__main__':
